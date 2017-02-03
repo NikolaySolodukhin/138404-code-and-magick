5372ad76eb2e24e978e45bcfe5d1d1cc55af3812
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2017-02-04 00:38:45 +0300

    Добавил role и aria. Сделал навигацию по Tab

--- js/setup.js
+++ js/setup.js
@@ -1,19 +1,91 @@
 'use strict';
-var openPopupChar = document.querySelector('.setup-open');
+
+var inputName = document.querySelector('.setup-user-name');
+
+inputName.setAttribute('required', true);
+inputName.setAttribute('maxLength', 50);
+inputName.setAttribute('minLength', 2);
+inputName.setAttribute('tabindex', 2);
+
 var setup = document.querySelector('.setup');
+var setupSubmit = setup.querySelector('.setup-submit');
+var openPopupChar = document.querySelector('.setup-open');
+var openPopupImg = openPopupChar.querySelector('.setup-open-icon');
 var closePopupChar = setup.querySelector('.setup-close');
+
+setup.setAttribute('role', 'dialog');
+
+closePopupChar.setAttribute('tabindex', 3);
+closePopupChar.setAttribute('role', 'button');
+
+openPopupImg.setAttribute('tabindex', 1);
+openPopupImg.setAttribute('role', 'button');
+
+setupSubmit.setAttribute('tabindex', 4);
 
 var wizardCoat = document.getElementById('wizard-coat');
 var wizardEyes = document.getElementById('wizard-eyes');
 
 var fireball = document.querySelector('.setup-fireball-wrap');
 
-openPopupChar.addEventListener('click', function () {
+
+var KEY_CODE = {
+  'enter': 13,
+  'escape': 27
+};
+
+var isActivateEvent = function (evt) {
+  return evt.keyCode && evt.keyCode === KEY_CODE.enter;
+};
+
+var setupKeydownHandler = function (evt) {
+  if (evt.keyCode === KEY_CODE.escape) {
+    setup.classList.add('invisible');
+  }
+};
+
+var ariaPressedChange = function () {
+  if (setup.classList.contains('invisible')) {
+    setupSubmit.attributes['aria-pressed'] = false;
+    openPopupImg.attributes['aria-pressed'] = false;
+    closePopupChar.attributes['aria-pressed'] = false;
+  } else {
+    setupSubmit.attributes['aria-pressed'] = true;
+    openPopupImg.attributes['aria-pressed'] = true;
+    closePopupChar.attributes['aria-pressed'] = true;
+  }
+};
+
+var showSetupElement = function () {
   setup.classList.remove('invisible');
+  document.addEventListener('keydown', setupKeydownHandler);
+  ariaPressedChange();
+};
+
+var hideSetupElement = function () {
+  setup.classList.add('invisible');
+  document.removeEventListener('keydown', setupKeydownHandler);
+  ariaPressedChange();
+};
+
+openPopupChar.addEventListener('click', function () {
+  showSetupElement();
 });
 
 closePopupChar.addEventListener('click', function () {
-  setup.classList.add('invisible');
+  hideSetupElement();
+});
+
+openPopupChar.addEventListener('keydown', function (evt) {
+  if (isActivateEvent(evt)) {
+    showSetupElement();
+  }
+});
+
+closePopupChar.addEventListener('keydown', function (evt) {
+  if (isActivateEvent(evt)) {
+    hideSetupElement();
+  }
 });
 
 var wizardCoatColors = [
