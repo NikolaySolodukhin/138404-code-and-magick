45f4e4c427e9d0d96a54973526676d41f35edb92
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2017-02-10 17:36:49 +0300

    Переписал модули используя IIFE

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,36 +1,39 @@
 'use strict';
+window.getColorElement = (function () {
+  return {
+    colorizeElement: function (element, colors, property) {
+      var getRandomColor = function () {
+        var currentColor = window.utils.getRandomElementExcept(colors, rgbHex(element.style[property]));
+        element.style[property] = currentColor;
+        window.setup.ariaPressedChange(element);
+      };
 
-window.colorizeElement = function (element, colors, property) {
-  var getRandomColor = function () {
-    var currentColor = window.utils.getRandomElementExcept(colors, rgbHex(element.style[property]));
-    element.style[property] = currentColor;
-    window.ariaPressedChange(element);
-  };
+      var getRandomColorByKeyborad = function (evt) {
+        if (window.setup.isActivateEvent(evt)) {
+          getRandomColor();
+          window.setup.ariaPressedChange(element);
+        }
+      };
 
-  var getRandomColorByKeyborad = function (evt) {
-    if (window.isActivateEvent(evt)) {
-      getRandomColor();
-      window.ariaPressedChange(element);
+      var rgbHex = function (color) {
+        var nums = /(.*?)rgb\((\d+),\s*(\d+),\s*(\d+)\)/i.exec(color);
+        if (!nums) {
+          return color;
+        }
+
+        var R = parseInt(nums[2], 10).toString(16);
+        var G = parseInt(nums[3], 10).toString(16);
+        var B = parseInt(nums[4], 10).toString(16);
+
+        return '#' + (
+          (R.length === 1 ? '0' + R : R) +
+          (G.length === 1 ? '0' + G : G) +
+          (B.length === 1 ? '0' + B : B)
+        );
+      };
+
+      element.addEventListener('keydown', getRandomColorByKeyborad);
+      element.addEventListener('click', getRandomColor);
     }
   };
-
-  var rgbHex = function (color) {
-    var nums = /(.*?)rgb\((\d+),\s*(\d+),\s*(\d+)\)/i.exec(color);
-    if (!nums) {
-      return color;
-    }
-
-    var R = parseInt(nums[2], 10).toString(16);
-    var G = parseInt(nums[3], 10).toString(16);
-    var B = parseInt(nums[4], 10).toString(16);
-
-    return '#' + (
-       (R.length === 1 ? '0' + R : R) +
-       (G.length === 1 ? '0' + G : G) +
-       (B.length === 1 ? '0' + B : B)
-     );
-  };
-
-  element.addEventListener('keydown', getRandomColorByKeyborad);
-  element.addEventListener('click', getRandomColor);
-};
+})();
--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,17 @@
 'use strict';
 
-window.utils = {
-  getRandomElement: function (array) {
-    return array[Math.floor(Math.random() * array.length)];
-  },
-  getRandomElementExcept: function (arr, arrElement) {
-    var currentColor = this.getRandomElement(arr);
+window.utils = (function () {
+  return {
+    getRandomElement: function (array) {
+      return array[Math.floor(Math.random() * array.length)];
+    },
+    getRandomElementExcept: function (arr, arrElement) {
+      var currentColor = this.getRandomElement(arr);
 
-    while (currentColor === arrElement) {
-      currentColor = this.getRandomElement(arr);
+      while (currentColor === arrElement) {
+        currentColor = this.getRandomElement(arr);
+      }
+      return currentColor;
     }
-    return currentColor;
-  }
-};
+  };
+})();
